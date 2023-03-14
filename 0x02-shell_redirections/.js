357f597445dc68a752cddc1c03f044495f3dd1e7
Babsky <abassdahood@gmail.com>
2023-03-14 04:41:44 -0700

    10-no_more_js

---
+++ 0x02-shell_redirections/.js
@@ -0,0 +1,16 @@
+.:
+total 52
+-rwxr--r-- 1 root root  32 Mar 14 02:43 0-hello_world
+-rwxr--r-- 1 root root  25 Mar 14 04:40 10-no_more_js
+-rwxr--r-- 1 root root  28 Mar 14 02:54 1-confused_smiley
+-rwxr--r-- 1 root root  29 Mar 14 03:07 2-hellofile
+-rwxr--r-- 1 root root  39 Mar 14 03:12 3-twofiles
+-rwxr--r-- 1 root root  29 Mar 14 03:32 4-lastlines
+-rwxr--r-- 1 root root  29 Mar 14 03:37 5-firstlines
+-rwxr--r-- 1 root root  36 Mar 14 03:56 6-third_line
+-rwxr--r-- 1 root root  52 Mar 14 04:17 7-file
+-rwxr--r-- 1 root root  36 Mar 14 04:24 8-cwd_state
+-rwxr--r-- 1 root root  35 Mar 14 04:33 9-duplicate_last_line
+-rw-r--r-- 1 root root   0 Mar 14 04:34 iacta
+-rw-r--r-- 1 root root 671 Mar 14 04:25 ls_cwd_content
+-rw-r--r-- 1 root root  41 Mar 14 02:40 README.md
